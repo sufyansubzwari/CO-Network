4e1c7ef3a2ada97e98a730d411a4ad45fd6517fe
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-23 14:49:42 -0400

    Testing react Title component

--- imports/ui/components/dumb/title/index.test.js
+++ imports/ui/components/dumb/title/index.test.js
@@ -5,21 +5,23 @@ import { expect } from "chai";
 import Title from "./index";
 
 import Adapter from 'enzyme-adapter-react-16';
+import { Meteor } from "meteor/meteor";
 
 configure({ adapter: new Adapter() });
+if(Meteor.isClient) {
+  describe("<Title />", () => {
 
-describe("<Title />", () => {
+    it("should be mount", () => {
+      const wrapper = mount(<Title>Hola</Title>);
+    });
+    it("should be render text", () => {
+      const wrapper = mount(<Title>Hola</Title>);
+      expect(wrapper.find('h1').text()).to.equal('Hola');
+    });
+    it("should have class", () => {
+      const wrapper = mount(<Title>Hola</Title>);
+      expect(wrapper.find('h1').hasClass('center')).to.equal(true);
+    });
 
-  it("should be mount", () => {
-    const wrapper = mount(<Title>Hola</Title>);
   });
-  it("should be render text", () => {
-    const wrapper = mount(<Title>Hola</Title>);
-    expect(wrapper.find('h1').text()).to.equal('Hola');
-  });
-  it("should have class", () => {
-    const wrapper = mount(<Title>Hola</Title>);
-    expect(wrapper.find('h1').hasClass('center')).to.equal(true);
-  });
-
-});
+}
