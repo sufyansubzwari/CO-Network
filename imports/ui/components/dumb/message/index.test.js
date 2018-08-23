e414c8d4ee7a4f607d24e5dc2a17ac37e36e7ca5
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-23 10:59:27 -0400

    Create message component unit test

---
+++ imports/ui/components/dumb/message/index.test.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { configure, mount } from 'enzyme';
+import { expect } from 'chai';
+import Message from './index';
+
+import Adapter from 'enzyme-adapter-react-16';
+import theme from '../../../theme';
+import { ThemeProvider } from 'styled-components';
+
+configure({ adapter: new Adapter() });
+
+describe("<Message />", () => {
+
+  it("should be mount", () => {
+    const wrapper = mount(
+      <ThemeProvider theme={theme}>
+          <Message type="error" content='Ops'/>
+      </ThemeProvider>
+    );
+  });
+  it("should be render text", () => {
+    const wrapper =mount(
+      <ThemeProvider theme={theme}>
+        <Message type="error" content='Ops'/>
+      </ThemeProvider>
+    );
+    expect(wrapper.find('div').text()).to.equal('Ops');
+  });
+
+});
