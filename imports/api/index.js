2d84db1405492d78c982e883b03468d466a61ea8
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-24 13:09:44 -0400

    Add project module

--- imports/api/index.js
+++ imports/api/index.js
@@ -4,5 +4,6 @@
 // see: /imports/entry-points/server/apollo-server/exec-schema.js
 export { default as Base } from './base';
 export { default as Users } from './users';
+export { default as Projects } from './projects';
 export { default as Constants } from './constants';
 export { default as ErrorHandling } from './error-handling';
