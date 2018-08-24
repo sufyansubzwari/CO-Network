57fb6e0838199a44ca004473f2749cccc0293a79
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-24 13:05:55 -0400

    Query for all project, project, and query parameters

--- imports/api/projects/server/resolvers/query.js
+++ imports/api/projects/server/resolvers/query.js
@@ -1,9 +1,27 @@
 import { Meteor } from "meteor/meteor";
 import Projects from "../../index";
 const Query = {};
-
-Query.project = (root, args, context) => {
-  return Projects.collection.find(args).fetch();
+Query.project = (root, { _id }, context) => {
+  if (_id) {
+    const project = Projects.collection.findOne(_id);
+    console.log(project);
+    return project;
+  }
+  throw new Error("Missing parameter");
+};
+Query.searchProject = (root, { project }, context) => {
+  let query = {};
+  if (project) {
+    query = project;
+  }
+  const projects = Projects.collection.find(query).fetch();
+  console.log(projects);
+  return projects;
+};
+Query.projects = (root, args, context) => {
+  const projects = Projects.collection.find({}).fetch();
+  console.log(projects);
+  return projects;
 };
 
 export default Query;
