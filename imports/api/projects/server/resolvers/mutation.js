f445811213164cd0b578c583dac3b8e5101baa86
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 11:55:57 -0400

    Add mutation for add member to project

--- imports/api/projects/server/resolvers/mutation.js
+++ imports/api/projects/server/resolvers/mutation.js
@@ -1,11 +1,16 @@
-import { Meteor } from "meteor/meteor";
 import Projects from "../../index";
 
 const Mutation = {};
 
 Mutation.saveProject = async (root, { project }, context) => {
-  const projectId=Projects.collection.insert(project);
+  const projectId = Projects.collection.insert(project);
   return Projects.collection.findOne(projectId);
+};
+Mutation.addMemberToProject = async (root, { project_id, use_id }, context) => {
+  await Projects.collection.update(project_id, {
+    $addToSet: { members: use_id }
+  });
+  return await Projects.collection.findOne(project_id);
 };
 
 export default Mutation;
