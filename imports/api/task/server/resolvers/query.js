788210fa81fd2c391b3b13e58873d2172cf65b89
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 13:25:55 -0400

    Remove console log in query

--- imports/api/task/server/resolvers/query.js
+++ imports/api/task/server/resolvers/query.js
@@ -1,12 +1,9 @@
 import { Meteor } from "meteor/meteor";
-import { delegateToSchema } from "graphql-tools";
-import userSchema from "../../../users/server/types.graphql";
 import Tasks from "../../index";
 const Query = {};
 Query.task = (root, { _id }, context) => {
   if (_id) {
     const task = Tasks.collection.findOne(_id);
-    console.log(task);
     return task;
   }
   throw new Error("Missing parameter");
@@ -17,30 +14,16 @@ Query.searchTask = (root, { task }, context) => {
     query = task;
   }
   const tasks = Tasks.collection.find(task).fetch();
-  console.log(tasks);
+
   return tasks;
 };
 Query.tasks = (root, args, context) => {
   const tasks = Tasks.collection.find({}).fetch();
-  console.log(tasks);
   return tasks;
 };
-Query.assignedUser = (root, args, context, info) => {
-  console.log("root", root);
-  console.log("context", context);
-  console.log("context", info);
-  const a = info.mergeInfo.delegateToSchema({
-    schema: userSchema,
-    operation: "query",
-    fieldName: "userBy",
-    args: {
-      _id: root.assigned
-    },
-    context,
-    info
-  });
-  console.log("context", a);
-  return a;
+Query.taskByProject = (root, { project_id }, context) => {
+  const tasks = Tasks.collection.find({ project_id: project_id }).fetch();
+  return tasks;
 };
 
 export default Query;
