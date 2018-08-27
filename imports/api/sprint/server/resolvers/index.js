05b4e42f545c28a603da18080f33a8265baaa552
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 12:56:07 -0400

    Add resolver for Sprint type

--- imports/api/sprint/server/resolvers/index.js
+++ imports/api/sprint/server/resolvers/index.js
@@ -1,10 +1,12 @@
 import Query from './query';
 import Mutation from './mutation';
+import Sprint from './sprint';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
+  Sprint
 };
 
 export default resolvers;
