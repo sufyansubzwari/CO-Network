0706741f6020958c29f64dba68149c38681a0b54
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 12:54:55 -0400

    Add project resolver

--- imports/api/projects/server/resolvers/index.js
+++ imports/api/projects/server/resolvers/index.js
@@ -1,10 +1,12 @@
 import Query from './query';
 import Mutation from './mutation';
+import Project from './project';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
+  Project
 };
 
 export default resolvers;
