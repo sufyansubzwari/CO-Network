2e2c9e9ced2e9f891135a4d4672ee0674cf03555
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 13:41:05 -0400

    Add resolver for project tasks

--- imports/api/projects/server/resolvers/project.js
+++ imports/api/projects/server/resolvers/project.js
@@ -1,21 +1,21 @@
-import Task from '../../../task';
-import Sprint from '../../../sprint';
-import ProjectCollection from '../../index';
+import Task from "../../../task";
+import Sprint from "../../../sprint";
+import ProjectCollection from "../../index";
 
 const Project = {};
 Project.totalPlannedHours = async project => {
   const pipeline = [
     {
       $match: {
-        project_id: project._id,
-      },
+        project_id: project._id
+      }
     },
     {
       $group: {
-        _id: '$project_id',
-        totalPlannedHours: { $sum: '$hours' },
-      },
-    },
+        _id: "$project_id",
+        totalPlannedHours: { $sum: "$hours" }
+      }
+    }
   ];
   const options = {};
   const data = await Task.collection
@@ -28,15 +28,15 @@ Project.totalLoggedHours = async project => {
   const pipeline = [
     {
       $match: {
-        project_id: project._id,
-      },
+        project_id: project._id
+      }
     },
     {
       $group: {
-        _id: '$project_id',
-        totalLoggedHours: { $sum: '$logHours' },
-      },
-    },
+        _id: "$project_id",
+        totalLoggedHours: { $sum: "$logHours" }
+      }
+    }
   ];
   const options = {};
   const data = await Task.collection
@@ -46,11 +46,14 @@ Project.totalLoggedHours = async project => {
   return data[0].totalLoggedHours;
 };
 Project.currentSprint = async project => {
-  const data= await Sprint.collection.find({ project_id: project._id, status:'Open' },
-    { sort: { starDate: -1 },limit:1},
-  ).fetch()
-  console.log(data)
-  return data[0]
+  const data = await Sprint.collection
+    .find(
+      { project_id: project._id, status: "Open" },
+      { sort: { starDate: -1 }, limit: 1 }
+    )
+    .fetch();
+  console.log(data);
+  return data[0];
 };
 Project.members = async project => {
   const pipeline = [
@@ -59,17 +62,16 @@ Project.members = async project => {
     },
     {
       $match: {
-        "_id": project._id,
+        _id: project._id
       }
     },
     {
-      $lookup:
-        {
-          from: "users",
-          localField: "members",
-          foreignField: "_id",
-          as: "members"
-        }
+      $lookup: {
+        from: "users",
+        localField: "members",
+        foreignField: "_id",
+        as: "members"
+      }
     }
   ];
   const options = {};
@@ -77,6 +79,9 @@ Project.members = async project => {
     .rawCollection()
     .aggregate(pipeline, options)
     .toArray();
-  return data[0].members
+  return data[0].members;
+};
+Project.tasks = async project => {
+  return await Task.collection.find({ project_id: project._id }).fetch();
 };
 export default Project;
