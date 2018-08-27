670b86b3f4711975373d5a9aa674876cfebed540
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 14:15:38 -0400

    Add task resolvers

--- imports/api/task/server/resolvers/index.js
+++ imports/api/task/server/resolvers/index.js
@@ -1,10 +1,12 @@
 import Query from './query';
 import Mutation from './mutation';
+import Task from './task';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
+  Task
 };
 
 export default resolvers;
