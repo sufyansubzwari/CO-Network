94e75139b7a151353da09d0d0956a98bb1ecdce2
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 09:46:58 -0400

    Index file for resolvers bug

---
+++ imports/api/bug/server/resolvers/index.js
@@ -0,0 +1,12 @@
+import Query from "./query";
+import Mutation from "./mutation";
+import Bug from "./bug";
+
+// Project namespace resolvers
+const resolvers = {
+  Query,
+  Mutation,
+  Bug
+};
+
+export default resolvers;
