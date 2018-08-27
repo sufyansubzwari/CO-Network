73bc2254e3130f03876326ab5cfa984ceb158f43
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-27 14:15:55 -0400

    Add query use by id

--- imports/api/users/server/resolvers/query.js
+++ imports/api/users/server/resolvers/query.js
@@ -1,12 +1,14 @@
-// Users namespace query resolvers
+import Users from "../../index";
 const Query = {};
 
 //------------------------------------------------------------------------------
 // We access to the current user here thanks to the context. The current
 // user is added to the context thanks to the 'meteor/apollo' package.
-Query.user = (root, args, context) => (
-  context.user
-);
+Query.user = (root, args, context) => context.user;
+Query.userBy = (root, { _id }, context) => {
+  return Users.collection.findOne(_id);
+};
+
 //------------------------------------------------------------------------------
 
 export default Query;
