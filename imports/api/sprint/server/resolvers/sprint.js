019ebd3dbc5bd89a62a0f43b94e17313a1e23036
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 17:50:49 -0400

    Add activeUsers resolvers in Sprint type

--- imports/api/sprint/server/resolvers/sprint.js
+++ imports/api/sprint/server/resolvers/sprint.js
@@ -1,48 +1,7 @@
-import Task from "../../../task";
-import Project from '../../../projects/server/resolvers/project';
-
+import Service from "../service";
 const Sprint = {};
-Sprint.totalPlannedHours = async sprint => {
-  const pipeline = [
-    {
-      $match: {
-        sprint_id: sprint._id
-      }
-    },
-    {
-      $group: {
-        _id: "$sprint_id",
-        totalPlannedHours: { $sum: "$hours" }
-      }
-    }
-  ];
-  const options = {};
-  const data = await Task.collection
-    .rawCollection()
-    .aggregate(pipeline, options)
-    .toArray();
-  return data[0].totalPlannedHours;
-};
-Sprint.totalLoggedHours = async sprint => {
-  const pipeline = [
-    {
-      $match: {
-        sprint_id: sprint._id
-      },
-    },
-    {
-      $group: {
-        _id: '$sprint_id',
-        totalLoggedHours: { $sum: '$logHours' },
-      },
-    },
-  ];
-  const options = {};
-  const data = await Task.collection
-    .rawCollection()
-    .aggregate(pipeline, options)
-    .toArray();
-  return data[0].totalLoggedHours;
+Sprint.activeUsers = sprint => {
+  return Service.activeUsers(sprint._id);
 };
 
 export default Sprint;
