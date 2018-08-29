c16c39e5d173e51ec71d1c53ffe5cd36c027b80e
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 15:21:02 -0400

    Add progress resolver

--- imports/api/projects/server/resolvers/report.js
+++ imports/api/projects/server/resolvers/report.js
@@ -1,5 +1,4 @@
 import Service from "../service";
-import Project from './project';
 const Report = {};
 Report.totalPlannedHours = project => {
   return Service.totalPlannedHours(project._id);
@@ -7,8 +6,11 @@ Report.totalPlannedHours = project => {
 Report.totalLoggedHours =  project => {
   return Service.totalLoggedHours(project._id);
 };
-Project.currentSprint =  project => {
-  return Service.currentSprint(project._id,);
+Report.currentSprint =  project => {
+  return Service.currentSprint(project._id);
+};
+Report.progress =  project => {
+  return Service.progress(project._id);
 };
 
 export default Report;
