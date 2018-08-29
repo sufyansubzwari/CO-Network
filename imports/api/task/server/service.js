1c9eb3d13a22f4000144c45f2422ae97dc08db14
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 13:58:38 -0400

    Add method for search task by project. Add method for search task by sprint. Add method for search task by requirement.

--- imports/api/task/server/service.js
+++ imports/api/task/server/service.js
@@ -1,11 +1,13 @@
 import Tasks from "../index";
 import * as _ from "lodash";
+import Requirements from '../../requirements';
 /**
  * @class Task Service
  * @summary Task Service with business logic
  */
 class TaskService {
   /**
+   * @name task
    * @summary function for save and update
    * @param {Object} data - Task information. If had id then this operation should be an update
    * @return {Object} Task
@@ -19,7 +21,7 @@ class TaskService {
       return Tasks.collection.findOne(data.id);
     }
   };
-  /**
+  /**@name totalPlannedHoursByProject
    * @summary Total planned hours by project
    * @param {Object} id - Project id
    * @return {Object} Return amount planned hours
@@ -45,6 +47,7 @@ class TaskService {
     return data[0].totalPlannedHours;
   };
   /**
+   * @name totalLoggedHoursByProject
    * @summary Total logged hours by project
    * @param {Object} id - Project id
    * @return {Object} Return amount logged hours
@@ -68,6 +71,33 @@ class TaskService {
       .aggregate(pipeline, options)
       .toArray();
     return data[0].totalLoggedHours;
+  };
+  /**
+   * @name taskByProject
+   * @summary Get all task by project
+   * @param {String} id - Project id
+   * @return {Object} Return all task by project
+   */
+  static taskByProject = async id => {
+    return await Tasks.collection.find({project_id:id}).fetch();
+  };
+  /**
+   * @name taskBySprint
+   * @summary Get all task by sprint
+   * @param {String} id - Project id
+   * @return {Object} Return all task by sprint
+   */
+  static taskBySprint = async id => {
+    return await Tasks.collection.find({sprint_id:id}).fetch();
+  };
+  /**
+   * @name taskByRequirement
+   * @summary Get all task by requirement
+   * @param {String} id - Project id
+   * @return {Object} Return all task by requirement
+   */
+  static taskByRequirement = async id => {
+    return await Tasks.collection.find({requirement_id:id}).fetch();
   };
 }
 
