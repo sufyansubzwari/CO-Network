ef7fa4d74532503d5b7851390bd716119d507f01
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 12:07:11 -0400

    Add sprint service for business logic

---
+++ imports/api/sprint/server/service.js
@@ -0,0 +1,41 @@
+import Sprints from "../index";
+import * as _ from "lodash";
+import Project from '../../projects/server/resolvers/project';
+/**
+ * @class Sprint Service
+ * @summary Sprint Service with business logic
+ */
+class SprintService {
+  /**
+   * @name sprint
+   * @summary function for save and update
+   * @param {Object} data - Sprint information. If had id then this operation should be an update
+   * @return {Object} Sprint
+   */
+  static sprint = async data => {
+    if (_.isUndefined(data.id)) {
+      const SprintId = Sprints.collection.insert(data);
+      return Sprints.collection.findOne(SprintId);
+    } else {
+      await Sprints.collection.update(data.id, { $set: data });
+      return Sprints.collection.findOne(data.id);
+    }
+  };
+  /**
+   * @name currentSprintByProject
+   * @summary Get current sprint by project
+   * @param {String} id - Sprint id
+   * @return {Object} Sprint
+   */
+  static currentSprintByProject = async id => {
+    const data = await Sprints.collection
+      .find(
+        { project_id: id, status: "Open" },
+      )
+      .fetch();
+    return data[0];
+  };
+
+}
+
+export default SprintService;
