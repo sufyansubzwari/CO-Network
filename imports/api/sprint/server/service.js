401ccd531640c9ce7a8758183eed6ec0ad8119dd
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 15:53:21 -0400

    Update service with getSprint,sprints,totalPlannedHours,totalLoggedHours and progress

--- imports/api/sprint/server/service.js
+++ imports/api/sprint/server/service.js
@@ -1,6 +1,8 @@
 import Sprints from "../index";
 import * as _ from "lodash";
-import Project from '../../projects/server/resolvers/project';
+import Tasks from '../../task';
+import Requirements from '../../requirements';
+import Projects from '../../projects';
 /**
  * @class Sprint Service
  * @summary Sprint Service with business logic
@@ -20,6 +22,24 @@ class SprintService {
       await Sprints.collection.update(data.id, { $set: data });
       return Sprints.collection.findOne(data.id);
     }
+  };
+  /**
+   * @name getSprint
+   * @summary Get sprint by id
+   * @param {String} _id - Sprint id
+   * @return {Object} Sprint
+   */
+  static getSprint = _id => {
+    return Sprints.collection.findOne(_id);
+  };
+  /**
+   *@name sprints
+   * @summary Get all sprints
+   * @param {Object} query - query parameters
+   * @return {Object}||[{Object }] Return one or all sprints
+   */
+  static sprints = query => {
+    return Projects.collection.find(query).fetch();
   };
   /**
    * @name currentSprintByProject
@@ -48,6 +68,34 @@ class SprintService {
       )
       .fetch();
     return data;
+  };
+  /**
+   * @name totalPlannedHours
+   * @summary Get amount planned hours in sprint
+   * @param {String} id - Sprint id
+   * @return {Number} Return amount planned hours
+   */
+  static totalPlannedHours = async id => {
+    return Tasks.service.totalPlannedHoursBySprint(id);
+  };
+  /**
+   * @name totalLoggedHours
+   * @summary Get amount logged hours in sprint
+   * @param {String} id - Sprint id
+   * @return {Number} Return amount logged hours
+   */
+  static totalLoggedHours = async id => {
+    return Tasks.service.totalLoggedHoursBySprint(id);
+  };
+
+  /**
+   * @name progress
+   * @summary Get sprint task status
+   * @param {String} id - Sprint id
+   * @return [{Object}] Return all sprints tasks status
+   */
+  static progress = async id => {
+    return Tasks.service.getTaskStatusBySprint(id);
   };
 
 }
