64785d3c20b762178553206965b00eb211dd200a
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 15:23:57 -0400

    Add totalLoggedHours,totalLoggedHours,requirements,progress to service

--- imports/api/requirements/server/service.js
+++ imports/api/requirements/server/service.js
@@ -1,5 +1,7 @@
 import Requirements from "../index";
 import * as _ from "lodash";
+import Projects from '../../projects';
+import Tasks from '../../task';
 /**
  * @class Requirement Service
  * @summary Requirement Service with business logic
@@ -21,6 +23,42 @@ class RequirementService {
     }
   };
   /**
+   * @name getRequirement
+   * @summary Get requirement by id
+   * @param {String} _id - Requirement id
+   * @return {Object} Requirement
+   */
+  static getRequirement = _id => {
+    return Requirements.collection.findOne(_id);
+  };
+  /**
+   *@name requirements
+   * @summary Get all requirements
+   * @param {Object} query - query parameters
+   * @return {Object}||[{Object }] Return one or all requirements
+   */
+  static requirements = query => {
+    return Projects.collection.find(query).fetch();
+  };
+  /**
+   * @name totalPlannedHours
+   * @summary Get amount planned hours in project
+   * @param {String} id - Project id
+   * @return {Number} Return amount planned hours
+   */
+  static totalPlannedHours = async id => {
+    return Tasks.service.totalPlannedHoursByProject(id);
+  };
+  /**
+   * @name totalLoggedHours
+   * @summary Get amount logged hours in project
+   * @param {String} id - Project id
+   * @return {Number} Return amount logged hours
+   */
+  static totalLoggedHours = async id => {
+    return Tasks.service.totalLoggedHoursByProject(id);
+  };
+  /**
    * @name  requirementByProject
    * @summary Get all requirement by project
    * @param {String} id - Project id
@@ -28,6 +66,15 @@ class RequirementService {
    */
   static requirementByProject = async id => {
     return await Requirements.collection.find({project_id:id}).fetch();
+  };
+  /**
+   * @name progress
+   * @summary Get requirement task status
+   * @param {String} id - Requirement id
+   * @return {Number} Return all requirement tasks status
+   */
+  static progress = async id => {
+    return Tasks.service.getTaskStatusByRequirement(id);
   };
 }
 
