ab04716ed6abd1330c0552ec609dac3db8196341
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 17:23:09 -0400

    Update resolvers index file

--- imports/api/requirements/server/resolvers/index.js
+++ imports/api/requirements/server/resolvers/index.js
@@ -1,12 +1,14 @@
 import Query from './query';
 import Mutation from './mutation';
 import Report from './report';
+import Requirement from './requirement';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
-  Report
+  Report,
+  Requirement
 };
 
 export default resolvers;
