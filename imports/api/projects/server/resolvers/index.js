b543dbf3c77792b4f57e2dd1cd7b98631225807a
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 11:59:37 -0400

    Add file resolvers for Report type

--- imports/api/projects/server/resolvers/index.js
+++ imports/api/projects/server/resolvers/index.js
@@ -1,12 +1,14 @@
 import Query from './query';
 import Mutation from './mutation';
 import Project from './project';
+import Report from './report';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
-  Project
+  Project,
+  Report
 };
 
 export default resolvers;
