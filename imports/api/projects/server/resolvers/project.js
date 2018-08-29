4cb96d8d70d18aeff837e8d6ec57605631024722
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 14:02:30 -0400

    Update task resolvers. Add resolver for requirements and sprints

--- imports/api/projects/server/resolvers/project.js
+++ imports/api/projects/server/resolvers/project.js
@@ -2,10 +2,16 @@ import Service from "../service";
 
 const Project = {};
 
-Project.members = async project => {
+Project.members = project => {
   return Service.members(project._id);
 };
-Project.tasks = async project => {
-  return await Task.collection.find({ project_id: project._id }).fetch();
+Project.sprints = project => {
+  return Service.sprints(project._id);
+};
+Project.requirements = project => {
+  return Service.requirements(project._id);
+};
+Project.tasks = project => {
+  return Service.tasks(project._id);
 };
 export default Project;
