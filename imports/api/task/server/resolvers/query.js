68369b6b6f73c7073df76cfdb16a6451000d0489
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 16:29:56 -0400

    Update task query resolvers

--- imports/api/task/server/resolvers/query.js
+++ imports/api/task/server/resolvers/query.js
@@ -1,29 +1,15 @@
-import { Meteor } from "meteor/meteor";
-import Tasks from "../../index";
+import Service from "../service"
 const Query = {};
 Query.task = (root, { _id }, context) => {
-  if (_id) {
-    const task = Tasks.collection.findOne(_id);
-    return task;
-  }
-  throw new Error("Missing parameter");
+  return Service.getTask(id)
 };
-Query.searchTask = (root, { task }, context) => {
+
+Query.tasks = (root, {task}, context) => {
   let query = {};
   if (task) {
     query = task;
   }
-  const tasks = Tasks.collection.find(task).fetch();
-
-  return tasks;
-};
-Query.tasks = (root, args, context) => {
-  const tasks = Tasks.collection.find({}).fetch();
-  return tasks;
-};
-Query.taskByProject = (root, { project_id }, context) => {
-  const tasks = Tasks.collection.find({ project_id: project_id }).fetch();
-  return tasks;
+  return Service.tasks(query);
 };
 
 export default Query;
