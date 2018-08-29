08b4902dcf3b066cd545d24350ec58f3d5b0a1d7
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 17:21:23 -0400

    Update variable name

--- imports/api/requirements/server/resolvers/report.js
+++ imports/api/requirements/server/resolvers/report.js
@@ -1,13 +1,13 @@
 import Service from "../service";
 const Report = {};
-Report.totalPlannedHours = project => {
-  return Service.totalPlannedHours(project._id);
+Report.totalPlannedHours = requirement => {
+  return Service.totalPlannedHours(requirement._id);
 };
-Report.totalLoggedHours =  project => {
-  return Service.totalLoggedHours(project._id);
+Report.totalLoggedHours =  requirement => {
+  return Service.totalLoggedHours(requirement._id);
 };
-Report.progress =  project => {
-  return Service.progress(project._id);
+Report.progress =  requirement => {
+  return Service.progress(requirement._id);
 };
 
 export default Report;
