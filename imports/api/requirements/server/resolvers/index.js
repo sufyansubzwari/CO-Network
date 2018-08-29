14074096f62cab5229f9bd574c522306400d128c
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-29 15:32:39 -0400

    Update index file for graphql resolver

--- imports/api/requirements/server/resolvers/index.js
+++ imports/api/requirements/server/resolvers/index.js
@@ -1,10 +1,12 @@
 import Query from './query';
 import Mutation from './mutation';
+import Report from './report';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
+  Report
 };
 
 export default resolvers;
