3a548827e211590b391e1aee88fbd6110d252677
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-30 15:50:08 -0400

    Add resolvers for tasks

--- imports/api/sprint/server/resolvers/sprint.js
+++ imports/api/sprint/server/resolvers/sprint.js
@@ -1,7 +1,18 @@
 import Service from "../service";
+import Requirement from '../../../requirements/server/resolvers/requirement';
 const Sprint = {};
 Sprint.activeUsers = sprint => {
   return Service.activeUsers(sprint._id);
 };
-
+Sprint.report = sprint => {
+  const object={
+    totalPlannedHours: Service.totalPlannedHours(sprint._id),
+    totalLoggedHours: Service.totalLoggedHours(sprint._id),
+    progress: Service.progress(sprint._id),
+  }
+  return object
+};
+Sprint.tasks = sprint => {
+  return Service.tasks(sprint._id);
+};
 export default Sprint;
