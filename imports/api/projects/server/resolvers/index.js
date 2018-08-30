179d114a08f1138569f51a1b96a4418bc4ddf0fd
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-30 15:44:15 -0400

    Remove no required Report resolvers

--- imports/api/projects/server/resolvers/index.js
+++ imports/api/projects/server/resolvers/index.js
@@ -1,14 +1,13 @@
 import Query from './query';
 import Mutation from './mutation';
 import Project from './project';
-import ReportProject from './report';
+
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
   Project,
-  ReportProject
 };
 
 export default resolvers;
