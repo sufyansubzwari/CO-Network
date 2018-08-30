fdc762cbad067361f0d32174fb244b8a00c06e03
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-30 15:47:53 -0400

    Add task resolvers to requirement

--- imports/api/requirements/server/resolvers/requirement.js
+++ imports/api/requirements/server/resolvers/requirement.js
@@ -1,7 +1,19 @@
 import Service from "../service";
+import Project from '../../../projects/server/resolvers/project';
 const Requirement = {};
 Requirement.status =  requirement => {
   return Service.status(requirement._id);
+};
+Requirement.report = requirement => {
+  const object={
+    totalPlannedHours: Service.totalPlannedHours(requirement._id),
+    totalLoggedHours: Service.totalLoggedHours(requirement._id),
+    progress: Service.progress(requirement._id),
+  }
+  return object
+};
+Requirement.tasks = requirement => {
+  return Service.tasks(requirement._id);
 };
 
 export default Requirement;
