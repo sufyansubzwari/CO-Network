b42ebe11dd9ca54ab045592eee7dd1891d1b9218
Victor Manuel Buil Vicente <vbuilvicente@gmail.com>
2018-08-30 09:39:55 -0400

    Update type Report name fro sprint

--- imports/api/sprint/server/resolvers/index.js
+++ imports/api/sprint/server/resolvers/index.js
@@ -1,13 +1,13 @@
 import Query from './query';
 import Mutation from './mutation';
-import Report from './report';
+import ReportSprint from './report';
 import Sprint from './sprint';
 
 // Project namespace resolvers
 const resolvers = {
   Query,
   Mutation,
-  Report,
+  ReportSprint,
   Sprint
 };
 
