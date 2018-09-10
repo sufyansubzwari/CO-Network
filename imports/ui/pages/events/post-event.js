4542e91aee99ee1b6d759ffd13a07fe8ab36e266
reynaldo <reybarcelo950@gmail.com>
2018-09-10 16:32:19 -0400

    [feature] put event view

--- imports/ui/pages/events/post-event.js
+++ imports/ui/pages/events/post-event.js
@@ -1,4 +1,8 @@
 import React, { Component } from "react";
+import { Container } from "btech-layout";
+import InternalLayout from "../../components/InternalLayout/InternalLayout";
+import PreviewEvent from "../../modules/event-module/preview";
+import EventForm from "../../modules/event-module/form";
 
 /**
  * @module Events
@@ -7,10 +11,24 @@ import React, { Component } from "react";
 class PostEvent extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      event: {}
+    };
   }
 
   render() {
-    return <div>sadasd</div>;
+    return (
+      <InternalLayout>
+        <Container>
+          <EventForm />
+        </Container>
+        <PreviewEvent
+          key={"rightSide"}
+          data={this.state.selectedItem}
+          index={this.state.selectedIndex}
+        />
+      </InternalLayout>
+    );
   }
 }
 
