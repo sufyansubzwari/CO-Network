1eb53c3b9bc3a0fc5e436c59a4474a144f3f8d94
Diogenes <diogenes.hern@gmail.com>
2018-09-20 17:33:09 -0400

    [User] adding mutation

--- imports/ui/pages/user/user-profile.js
+++ imports/ui/pages/user/user-profile.js
@@ -1,12 +1,12 @@
-import React, { Component } from "react";
-import { Layout, Container } from "btech-layout";
+import React, {Component} from "react";
+import {Layout, Container} from "btech-layout";
 import UserForm from "./../../modules/user-module/form/";
 import InternalLayout from "../../components/InternalLayout/InternalLayout";
-import { Preview } from "../../../ui/components";
+import {Preview} from "../../../ui/components";
 import UserPreviewBody from "../../components/Preview/UserPreviewBody";
-import { Mutation } from "react-apollo";
-import { withRouter } from "react-router-dom";
-import { CreateEvent } from "../../apollo-client/event";
+import {Mutation} from "react-apollo";
+import {withRouter} from "react-router-dom";
+import {CreateUser} from "../../apollo-client/user";
 
 /**
  * @module User
@@ -17,54 +17,54 @@ class UserProfile extends Component {
     super(props);
 
     let user = {
-            ...props.curUser.profile,
-            info: {
-                name: "",
-                lastName: "",
-                email: "",
-                website: "",
-                location: {
-                    address: "",
-                    location: {lat: "", lng: ""},
-                    fullLocation: {}
-                }
-            },
-            social: {
-                github: "",
-                facebook: "",
-                twitter: "",
-                google: ""
-            },
-            aboutMe: {
-                yourPassion: "",
-                existingProblem: "",
-                steps: ""
-            },
-            knowledge: {
-                languages: [],
-                curiosity: [],
-                lookingFor: []
-            },
-            professional: {
-                salaryRange: {
-                    min: "",
-                    max: ""
-                },
-                jobType: [],
-                industry: []
-            },
-            speaker: {
-                lookingFor: [],
-                topic: [],
-                style: [],
-                stage: [],
-                otherlooking: [],
-                otherpreferred: []
-            }
+      ...props.curUser.profile,
+      info: {
+        name: "",
+        lastName: "",
+        email: "",
+        website: "",
+        location: {
+          address: "",
+          location: {lat: "", lng: ""},
+          fullLocation: {}
         }
+      },
+      social: {
+        github: "",
+        facebook: "",
+        twitter: "",
+        google: ""
+      },
+      aboutMe: {
+        yourPassion: "",
+        existingProblem: "",
+        steps: ""
+      },
+      knowledge: {
+        languages: [],
+        curiosity: [],
+        lookingFor: []
+      },
+      professional: {
+        salaryRange: {
+          min: "",
+          max: ""
+        },
+        jobType: [],
+        industry: []
+      },
+      speaker: {
+        lookingFor: [],
+        topic: [],
+        style: [],
+        stage: [],
+        otherlooking: [],
+        otherpreferred: []
+      }
+    }
 
     this.state = {
-        user: user
+      user: user
     }
 
     this.handleBackgroundChange = this.handleBackgroundChange.bind(this)
@@ -75,39 +75,51 @@ class UserProfile extends Component {
     this.props.history.push(`/`);
   }
 
-  handleBackgroundChange(src){
-      this.setState({
-          user: {
-              ...this.state.user,
-              cover: src
-          }})
+  handleBackgroundChange(src) {
+    this.setState({
+      user: {
+        ...this.state.user,
+        cover: src
+      }
+    })
   }
 
-    handleUserPhotoChange(src){
+  handleUserPhotoChange(src) {
 
-      this.setState({
-            user: {
-                ...this.state.user,
-                image: src
-            }})
-    }
+    this.setState({
+      user: {
+        ...this.state.user,
+        image: src
+      }
+    })
+  }
 
   render() {
     return (
       <InternalLayout>
         <Container fullY key={"leftSide"}>
-          <UserForm
-            onFinish={data => this.onPostAction(() => console.log(createProfile), data)}
-            onCancel={() => this.onCancel()}
-            userLogged={false}
-            handleChangeProfile={(user) => this.setState({user: {...this.state.user,...user}})}
-            user={this.state.user}
-            {...this.props}
-          />
+          <Mutation
+            mutation={CreateUser}
+            onCompleted={() =>
+              this.props.history.push("/", {userCreate: true})
+            }
+            onError={error => console.log("Error: ", error)}
+          >
+            {(createProfile, {profileCreated}) => (
+              <UserForm
+                onFinish={data => this.onPostAction(() => console.log(createProfile), data)}
+                onCancel={() => this.onCancel()}
+                userLogged={false}
+                handleChangeProfile={(user) => this.setState({user: {...this.state.user, ...user}})}
+                user={this.state.user}
+                {...this.props}
+              />
+            )}
+          </Mutation>
         </Container>
         <Preview
           showAvatar={true}
-          image={ this.state.user && this.state.user.image}
+          image={this.state.user && this.state.user.image}
           key={"rightSide"}
           navClicked={index => console.log(index)}
           navOptions={[
@@ -117,18 +129,18 @@ class UserProfile extends Component {
               checkVisibility: () => {
                 return this.state.selectedItem && this.state.selectedItem.id;
               },
-              onClick: function() {
+              onClick: function () {
                 console.log("Remove");
               }
             }
           ]}
           index={this.state.selectedIndex}
           data={this.state.selectedItem}
-          backGroundImage={ this.state.user && this.state.user.cover }
-          onBackgroundChange={ this.handleBackgroundChange }
-          onUserPhotoChange={ this.handleUserPhotoChange }
+          backGroundImage={this.state.user && this.state.user.cover}
+          onBackgroundChange={this.handleBackgroundChange}
+          onUserPhotoChange={this.handleUserPhotoChange}
         >
-          <UserPreviewBody user={this.state.user} />
+          <UserPreviewBody user={this.state.user}/>
         </Preview>
       </InternalLayout>
     );
