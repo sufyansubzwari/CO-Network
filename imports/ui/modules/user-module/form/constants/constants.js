86703e095f0660094a3ccb2a4e566af15052c450
Javier Ponce <jponceprez@gmail.com>
2018-09-24 18:28:53 +0200

    fixed the signup checkboxes not updating

--- imports/ui/modules/user-module/form/constants/constants.js
+++ imports/ui/modules/user-module/form/constants/constants.js
@@ -81,7 +81,7 @@ export const SIGNUP_TEXT_3 =
   "By registering for the “CO Network”, you are entering a binding agreement to exercise your passion and creativity on the world’s biggest challenges.";
 
 export const SIGNUP_OPTIONS = [
-  { label: "Innovation emerges from unrestrained collaboration" },
+  { label: "Innovation emerges from unrestrained collaboration"},
   { label: "The most valuable currency is rigorous curiosity" },
   { label: "Every interaction moves towards something created" }
   // {label: 'I agree to uphold the values, culture and ethics outlined in the CO Network’s Terms and Conditions, GDPR Compliant Privacy Policy, Anti-Spam Policy agreement.'},
