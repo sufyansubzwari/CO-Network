9b1266f466ff7180679375394f0ebe96d97c5af1
reynaldo <reybarcelo950@gmail.com>
2018-09-26 15:51:01 -0400

    [feature] upload to S3 the products services files

---
+++ imports/ui/modules/event-module/form/constants/community-event-categories.js
@@ -0,0 +1,10 @@
+export const COMMUNITYEVENTCATEGORIES = [
+  { label: "Co-Founders for Startup", value: "Co-Founders for Startup" },
+  { label: "Competition Teammates" , value: "Competition Teammates" },
+  { label: "Individuals to Mentor" , value: "Individuals to Mentor"},
+  { label: "Mentorship" , value: "Mentorship"},
+  { label: "Tech Project Partners" , value: "Tech Project Partners"},
+  { label: "Data Trading" , value: "Data Trading"},
+  { label: "Scientific Collaborations" , value: "Scientific Collaborations"},
+  { label: "Study Group Companions" , value: "Study Group Companions"}
+];
