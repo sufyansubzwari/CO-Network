f107b5b4bf23cbab604cf08e2d1afdc2a2335b3e
Diogenes <diogenes.hern@gmail.com>
2018-09-26 00:14:48 -0400

    [Tags] added tags suggestion for curiosity tags!!

--- imports/ui/modules/user-module/form/Steps/ThirdStep/index.js
+++ imports/ui/modules/user-module/form/Steps/ThirdStep/index.js
@@ -4,16 +4,11 @@ import {
   InputAutoComplete,
   TagList
 } from "btech-base-forms-component";
-import {Container, Layout} from "btech-layout";
+import { Container, Layout } from "btech-layout";
 
-import {
-
-  LOOKING_FOR_DEFAULT
-  ,
-  TAG_LEVEL
-} from "../../../../../constants";
-import {GetTags} from "../../../../../apollo-client/tag";
-import {Query} from "react-apollo";
+import { LOOKING_FOR_DEFAULT, TAG_LEVEL } from "../../../../../constants";
+import { GetTags } from "../../../../../apollo-client/tag";
+import { Query } from "react-apollo";
 
 class ThirdStep extends React.Component {
   constructor(props) {
@@ -47,14 +42,14 @@ class ThirdStep extends React.Component {
     const lookings = selected.filter(element => element.active);
     const temp = this.state.user;
     temp["knowledge"]["lookingFor"] = lookings;
-    this.setState({lookingfor: selected, user: temp}, () =>
+    this.setState({ lookingfor: selected, user: temp }, () =>
       this.notifyParent()
     );
   }
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.data && nextProps.data !== this.state.user)
-      this.setState({user: nextProps.data});
+      this.setState({ user: nextProps.data });
   }
 
   notifyParent(model, name, value) {
@@ -62,7 +57,7 @@ class ThirdStep extends React.Component {
       let user = this.state.user;
       user[name] = value;
       this.setState(
-        {user: user},
+        { user: user },
         () => this.props.onChange && this.props.onChange(this.state.user)
       );
     } else this.props.onChange && this.props.onChange(this.state.user);
@@ -70,20 +65,20 @@ class ThirdStep extends React.Component {
 
   onAddTags(name, type, tag) {
     if (tag.label && tag.label.length > 0) {
-      let newTag = Object.assign({}, {tag: {...tag}});
+      let newTag = Object.assign({}, { tag: { ...tag } });
       let tags =
         (this.state.user.knowledge && this.state.user.knowledge[name]) || [];
       !newTag.tag.name ? (newTag.tag.name = newTag.tag.label) : null;
       newTag.tag.type = type;
       tags.push(newTag);
       this.state.user.knowledge[name] = tags;
-      this.setState({user: this.state.user}, () => this.notifyParent());
+      this.setState({ user: this.state.user }, () => this.notifyParent());
     }
   }
 
   onCloseTags(e, tag, index, name) {
     this.state.user.knowledge[name].splice(index, 1);
-    this.setState({user: this.state.user}, () => this.notifyParent());
+    this.setState({ user: this.state.user }, () => this.notifyParent());
   }
 
   handleCategoryChange(index, value, color, icon, name) {
@@ -100,24 +95,32 @@ class ThirdStep extends React.Component {
         level: value
       };
       this.state.user.knowledge[name][index] = newTag;
-      this.setState({user: this.state.user}, () => this.notifyParent());
+      this.setState({ user: this.state.user }, () => this.notifyParent());
     }
   }
 
-  tagsSuggested(tags) {
+  tagsSuggested(tags, name) {
     let sug = JSON.parse(JSON.stringify(tags));
-    return sug.sort((a, b) => b.used - a.used).map(tag => ({
-      ...tag,
-      active: this.state.user && this.state.user.knowledge && this.state.user.knowledge.languages.findIndex(item => item.tag._id === tag._id) > -1
-    })).slice(0, 5);
+    return sug
+      .sort((a, b) => b.used - a.used)
+      .map(tag => ({
+        ...tag,
+        active:
+          this.state.user &&
+          this.state.user.knowledge &&
+          this.state.user.knowledge[name].findIndex(
+            item => item.tag._id === tag._id
+          ) > -1
+      }))
+      .slice(0, 5);
   }
 
   render() {
     return (
       <Layout rowGap={"25px"}>
         <Container>
-          <Query query={GetTags} variables={{tags: {type: "Languages"}}}>
-            {({loading, error, data}) => {
+          <Query query={GetTags} variables={{ tags: { type: "Languages" } }}>
+            {({ loading, error, data }) => {
               if (loading) return <div>Fetching</div>;
               if (error) return <div>Error</div>;
               return (
@@ -135,20 +138,21 @@ class ThirdStep extends React.Component {
                       "Languages"
                     )}
                     options={data.tags}
-                    model={{others: []}}
+                    model={{ others: [] }}
                     name={"others"}
                   />
                   <Container mt={"10px"}>
                     <TagList
-                      tags={this.tagsSuggested(data.tags)}
+                      tags={this.tagsSuggested(data.tags, "languages")}
                       onSelect={(event, tag, index) => {
                         if (!tag.active) {
                           delete tag.active;
-                          this.onAddTags("languages", "Languages", tag)
-                        }
-                        else {
-                          const pos = this.state.user.knowledge.languages.findIndex(item => item.tag._id === tag._id);
-                          this.onCloseTags(event, tag, pos, "languages")
+                          this.onAddTags("languages", "Languages", tag);
+                        } else {
+                          const pos = this.state.user.knowledge.languages.findIndex(
+                            item => item.tag._id === tag._id
+                          );
+                          this.onCloseTags(event, tag, pos, "languages");
                         }
                       }}
                     />
@@ -163,14 +167,14 @@ class ThirdStep extends React.Component {
                 this.state.user.knowledge.languages &&
                 this.state.user.knowledge.languages.length > 0
                   ? this.state.user.knowledge.languages.map(item => ({
-                    active: true,
-                    useIcon: true,
-                    levelColor: item.levelColor || "",
-                    icon: item.icon || "",
-                    level: item.level || "",
-                    ...item.tag,
-                    showOptions: !item.levelColor
-                  }))
+                      active: true,
+                      useIcon: true,
+                      levelColor: item.levelColor || "",
+                      icon: item.icon || "",
+                      level: item.level || "",
+                      ...item.tag,
+                      showOptions: !item.levelColor
+                    }))
                   : []
               }
               closeable={true}
@@ -178,32 +182,58 @@ class ThirdStep extends React.Component {
                 this.onCloseTags(e, tag, index, "languages")
               }
               levelOptions={TAG_LEVEL}
-              onCategoryChange={(index, value, color, icon) => this.handleCategoryChange(index, value, color, icon, 'languages')}
+              onCategoryChange={(index, value, color, icon) =>
+                this.handleCategoryChange(
+                  index,
+                  value,
+                  color,
+                  icon,
+                  "languages"
+                )
+              }
             />
           </Container>
         </Container>
         <Container>
-          <Query query={GetTags} variables={{tags: {type: "Curiosity"}}}>
-            {({loading, error, data}) => {
+          <Query query={GetTags} variables={{ tags: { type: "Curiosity" } }}>
+            {({ loading, error, data }) => {
               if (loading) return <div>Fetching</div>;
               if (error) return <div>Error</div>;
               return (
-                <InputAutoComplete
-                  placeholderText={"Curiosity & Experience"}
-                  getAddedOptions={this.onAddTags.bind(
-                    this,
-                    "curiosity",
-                    "Curiosity"
-                  )}
-                  getNewAddedOptions={this.onAddTags.bind(
-                    this,
-                    "curiosity",
-                    "Curiosity"
-                  )}
-                  options={data.tags}
-                  model={{others: []}}
-                  name={"others"}
-                />
+                <div>
+                  <InputAutoComplete
+                    placeholderText={"Curiosity & Experience"}
+                    getAddedOptions={this.onAddTags.bind(
+                      this,
+                      "curiosity",
+                      "Curiosity"
+                    )}
+                    getNewAddedOptions={this.onAddTags.bind(
+                      this,
+                      "curiosity",
+                      "Curiosity"
+                    )}
+                    options={data.tags}
+                    model={{ others: [] }}
+                    name={"others"}
+                  />
+                  <Container mt={"10px"}>
+                    <TagList
+                      tags={this.tagsSuggested(data.tags, "curiosity")}
+                      onSelect={(event, tag, index) => {
+                        if (!tag.active) {
+                          delete tag.active;
+                          this.onAddTags("curiosity", "Curiosity", tag);
+                        } else {
+                          const pos = this.state.user.knowledge.curiosity.findIndex(
+                            item => item.tag._id === tag._id
+                          );
+                          this.onCloseTags(event, tag, pos, "curiosity");
+                        }
+                      }}
+                    />
+                  </Container>
+                </div>
               );
             }}
           </Query>
@@ -213,14 +243,14 @@ class ThirdStep extends React.Component {
                 this.state.user.knowledge.curiosity &&
                 this.state.user.knowledge.curiosity.length > 0
                   ? this.state.user.knowledge.curiosity.map(item => ({
-                    active: true,
-                    useIcon: true,
-                    levelColor: item.levelColor || "",
-                    icon: item.icon || "",
-                    level: item.level || "",
-                    ...item.tag,
-                    showOptions: !item.levelColor,
-                  }))
+                      active: true,
+                      useIcon: true,
+                      levelColor: item.levelColor || "",
+                      icon: item.icon || "",
+                      level: item.level || "",
+                      ...item.tag,
+                      showOptions: !item.levelColor
+                    }))
                   : []
               }
               closeable={true}
@@ -228,7 +258,15 @@ class ThirdStep extends React.Component {
                 this.onCloseTags(e, tag, index, "curiosity")
               }
               levelOptions={TAG_LEVEL}
-              onCategoryChange={(index, value, color, icon) => this.handleCategoryChange(index, value, color, icon, 'curiosity')}
+              onCategoryChange={(index, value, color, icon) =>
+                this.handleCategoryChange(
+                  index,
+                  value,
+                  color,
+                  icon,
+                  "curiosity"
+                )
+              }
             />
           </Container>
         </Container>
