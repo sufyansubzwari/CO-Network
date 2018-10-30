c300823f95a48a851ada9414fdc8ca62683a521e
Diogenes <diogenes.hern@gmail.com>
2018-10-30 16:56:43 -0400

    [notificationToast] setting notification style!

---
+++ imports/ui/services/NotificationToast/components/types.js
@@ -0,0 +1,20 @@
+export const types = {
+  success: "Nice, take a break!",
+  error: "Upps, Error!",
+  warn: "Warning!",
+  info: "Heads Up!"
+};
+
+export const iconTypes = {
+  success: "coffee",
+  error: "close-circle",
+  warn: "traffic",
+  info: "flash"
+};
+
+export const colorsTypes = {
+  success: "coffee",
+  error: "#EB3939",
+  warn: "#EBA639",
+  info: "#3991EB"
+};
