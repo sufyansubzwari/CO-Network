5ddb77934805cbb255ec7eb999964f07c94937da
Ubuntu <ubuntu@ip-172-31-37-254.ec2.internal>
2018-11-05 18:52:32 +0000

    Uploaded from Gitlab

--- imports/ui/constants/regex.constant.js
+++ imports/ui/constants/regex.constant.js
@@ -1,3 +1,3 @@
-export const EMAIL_REGEX = /[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$/;
+export const EMAIL_REGEX = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,3}$/;
 export const PHONE_REGEX = /^\+?([0-9]{2})\)?[-. ]?([0-9]{4})[-. ]?([0-9]{4})$/;
 export const NAME_REGEX = /^[a-zA-ZA-zÀ-ú0-9'. ]{1,40}$/;
